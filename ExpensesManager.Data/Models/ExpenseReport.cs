526889edd3cefebdd479231a3358c95394b23016
Brian <briannokanga@gmail.com>
2022-03-03 17:18:22 +0300

    Added Data Annotations to the model

--- ExpensesManager.Data/Models/ExpenseReport.cs
+++ ExpensesManager.Data/Models/ExpenseReport.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,23 @@ namespace ExpensesManager.Data.Models
 {
     public class ExpenseReport
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(10, 2)")]
         public decimal Amount { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required]
         public DateTime ExpenseDate { get; set; } = DateTime.Now;
+
+        [Required]
         public string Category { get; set; }
     }
 }

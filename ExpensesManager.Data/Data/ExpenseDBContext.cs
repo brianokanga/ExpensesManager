1260a3a8a6167989c16fdf9916fc1180a5dfa46e
Brian <briannokanga@gmail.com>
2022-03-03 16:59:33 +0300

    Added DbContext

---
+++ ExpensesManager.Data/Data/ExpenseDBContext.cs
@@ -0,0 +1,23 @@
+﻿using ExpensesManager.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesManager.Data.Data
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Your connection string");
+            }
+        }
+    }
+}

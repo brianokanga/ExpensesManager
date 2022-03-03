f6c36c01000a99d9396a73d42e752052b14e9e87
Brian <briannokanga@gmail.com>
2022-03-03 15:28:54 +0300

    Add project files.

---
+++ ExpensesManager.Data/Models/ExpenseReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesManager.Data.Models
+{
+    public class ExpenseReport
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ExpenseDate { get; set; } = DateTime.Now;
+        public string Category { get; set; }
+    }
+}
